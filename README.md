0a8f3dbe23d017df9a1d659ded7ae5f98d9f88fe
Rohyl Joshi <rohyl.joshi@unimelb.edu.au>
2020-03-29 13:36:07 +1100

    added boilerplate code for workshop-3

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-rohylj-workshops
+# rohyl-workshops
+
---
+++ workshop-3/src/StringScoring.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class StringScoring {
+    public static void main(String[] args) {
+        String testCase = new Scanner(System.in).nextLine();
+        // Feel free to do whatever you want from here, just make sure your output is
+        // printed to System.out
+        System.out.println("Replace this with your test case output");
+    }
+}
