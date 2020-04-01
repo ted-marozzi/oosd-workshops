e627df3d981107fcb5d86b442544770721e8ab5f
TedMarozzi <marozzi.t@gmail.com>
2020-04-01 12:59:27 +1100

    Implemented most basic functinality of SctringScoring

--- workshop-3/src/StringScoring.java
+++ workshop-3/src/StringScoring.java
@@ -1,10 +1,67 @@
 import java.util.Scanner;
 
+class Scorer
+{
+    private final int alphabetLen = 26;
+    private Letter[] alphabetArr = new Letter[alphabetLen];
+    private int[] scoreArr = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+    private int asciiValue = 65;
+    private final int asciiOffset = 65;
+    private int score = 0;
+
+    public Scorer()
+    {
+        for(int i = 0; i< alphabetLen; i++)
+        {
+            alphabetArr[i] = new Letter(asciiValue++, scoreArr[i]);
+        }
+    }
+
+    public int outputScore(String string)
+    {
+
+        string = string.toUpperCase();
+        char[] chars = string.toCharArray();
+
+        for(char character:chars)
+        {
+            int characterIndex = (byte)character - asciiOffset;
+            score = score + alphabetArr[characterIndex].getScore();
+        }
+        return score;
+    }
+
+
+}
+
+class Letter
+{
+    private int score;
+    private char symbol;
+
+
+    public Letter(int asciiValue, int score)
+    {
+        this.symbol = (char)asciiValue;
+        this.score = score;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+}
+
 public class StringScoring {
     public static void main(String[] args) {
         String testCase = new Scanner(System.in).nextLine();
         // Feel free to do whatever you want from here, just make sure your output is
         // printed to System.out
-        System.out.println("Replace this with your test case output");
+
+        Scorer scorer = new Scorer();
+
+
+        System.out.println(scorer.outputScore(testCase));
     }
 }
--- workshop-3/src/TvNetwork.java
+++ workshop-3/src/TvNetwork.java
@@ -1,26 +1,70 @@
-public class Show
-{
+class Show {
     private String name;
     private String airTime;
 
-    public Show(String name, int hour, int minute)
-    {
-        if(hour < 0 || hour > 24)
-        {
-            System.out.println("Warning: invalid hour: " + hour);
+    public Show(String name, int hour, int minute) {
+        if (hour < 0 || hour > 24) {
+            System.out.println("warning: invalid hour: " + hour);
         }
-        if(minute < 0 || minute > 59)
-        {
-            System.out.println("Warning: invalid minute: " + minute);
+        if (minute < 0 || minute > 59) {
+            System.out.println("warning: invalid minute: " + minute);
         }
 
         this.name = name;
-        // TODO: sting format
-        //airTime = String.format();
+        airTime = String.format("%02d:%02d", hour, minute);
     }
+
+    public String toString() {
+        return String.format("%s airing at %s", name, airTime);
+    }
+}
+
+class Channel
+{
+    private final String name;
+    private Show[] shows = new Show[5];
+    private int numShows = 0;
+
+    public Channel(String name)
+    {
+        this.name = name;
+
+
+    }
+
+    public void createShow(String name, int hour, int minute)
+    {
+        Show show = new Show(name, hour, minute);
+        System.out.println(show);
+
+
+        shows[numShows] = show;
+        numShows++;
+    }
+
+    public String toString()
+    {
+        String channelString = "";
+        String showString = "";
+
+        for(int i=0; i < this.numShows; i++)
+        {
+            showString = shows[i].toString();
+            channelString = channelString + showString;
+        }
+        return channelString;
+
+    }
+
 }
 
 public class TvNetwork
 {
+    public static void main(String[] args) {
 
+        Channel channel7 = new Channel("Channel 7");
+        channel7.createShow("Channel 7 News", 7, 30);
+        channel7.createShow("Channel 7 Sport", 8, 30);
+        channel7.toString();
+    }
 }
