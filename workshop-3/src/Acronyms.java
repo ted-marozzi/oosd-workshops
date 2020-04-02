43a66c69fe2ec5771e635c348442835387ff8839
TedMarozzi <marozzi.t@gmail.com>
2020-04-02 12:50:13 +1100

    Completed Acronmys basic functionality, need to add comments

--- workshop-3/src/Acronyms.java
+++ workshop-3/src/Acronyms.java
@@ -1,10 +1,40 @@
+/*
+ * Student id: 910913
+ *
+ * Student Name: Edward Marozzi
+ *
+ * Aim:
+ *
+ * Basic Structure:
+ */
+
 import java.util.Scanner;
+
+class AcroMachine
+{
+    //private final String input;
+
+    public String outputAcro(String input)
+    {
+        String[] words = input.split(" ");
+        char firstLetter;
+        StringBuilder output = new StringBuilder();
+        for(String word:words)
+        {
+            firstLetter = Character.toUpperCase(word.charAt(0));
+            output.append(firstLetter);
+        }
+        return output.toString();
+    }
+
+}
 
 public class Acronyms {
     public static void main(String[] args) {
         String testCase = new Scanner(System.in).nextLine();
         // Feel free to do whatever you want from here, just make sure your output is
         // printed to System.out
-        System.out.println("Replace this with your test case output");
+        AcroMachine acroMachine = new AcroMachine();
+        System.out.println(acroMachine.outputAcro(testCase));
     }
 }
