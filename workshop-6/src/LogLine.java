6265126c77c5fe85f6d14e20260dc3a3b5524fa9
Ted <marozzi.t@gmail.com>
2020-05-10 22:57:16 +1000

    Implemented a compareTo method and constructor for LogLine class, the compareTo methods sorts by arrival time then depatrture city then arrival city, then I added a buildLogline method that returns the deconstructed log line string back to a reconstructed string. I just realised I could store the string and return it, will do that next commit.

--- workshop-6/src/LogLine.java
+++ workshop-6/src/LogLine.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+public class LogLine implements Comparable<LogLine>
+{
 
-public class logLine {
+    private String arrCity = null;
+    private String depCity = null;
+    private int arrTime = 0;
+    private int userID;
+
+    public LogLine(String logLine)
+    {
+        String[] logLine1 = logLine.split(":");
+        userID = Integer.parseInt(logLine1[0]);
+        depCity = logLine1[1];
+        arrCity = logLine1[2];
+        arrTime = Integer.parseInt(logLine1[3]);
+    }
+
+
+    @Override
+    public int compareTo(LogLine logLine) {
+        if(this.arrTime == logLine.arrTime)
+        {
+            if(this.depCity.compareTo(logLine.depCity) == 0)
+            {
+                return this.arrCity.compareTo(logLine.arrCity);
+
+            }
+            else
+                return this.depCity.compareTo(logLine.depCity);
+        }
+        else
+            return Integer.compare(this.arrTime , logLine.arrTime);
+    }
+
+    public String buildLogline()
+    {
+        StringBuilder logLine = new StringBuilder();
+        logLine.append(userID).append(":").append(depCity).append(":").append(arrCity).append(":").append(arrTime);
+        return logLine.toString();
+    }
+
 }
