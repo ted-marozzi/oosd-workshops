0ed241512cdf2bc26c106ea74ed74da7dde57ddd
Ted <marozzi.t@gmail.com>
2020-05-28 21:20:50 +1000

    FIninsed workshop 9

--- workshop-9/src/Colour.java
+++ workshop-9/src/Colour.java
@@ -4,10 +4,20 @@ public final class Colour {
     private final int g;
     private final int b;
 
-    public Colour(int r, int g, int b) {
+    private static Colour rgb;
+
+    private Colour(int r, int g, int b) {
         this.r = r;
         this.g = g;
         this.b = b;
+    }
+
+    public static Colour fromRGB(int r, int g, int b) {
+        if(r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255)
+        {
+            throw new IllegalArgumentException("Invalid RGB value specified");
+        }
+        return rgb = new Colour(r,g,b);
     }
 
     public int getR() {
@@ -21,5 +31,6 @@ public final class Colour {
     public int getB() {
         return b;
     }
+
 
 }
